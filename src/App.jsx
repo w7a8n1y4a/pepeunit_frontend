a149e505228ba9c767057d88eb61bd97aca897ec
Ivan Serebrennikov <admin@silberworks.com>
2024-08-02 00:27:05 +0300

    feat(env): add env, change settings vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,35 @@ import { useState } from 'react'
 import ForceGraph from './components/ForceGraph'
 import './App.css'
 
+// import request from 'graphql-request'
+// import { useQuery } from '@tanstack/react-query'
+// import { graphql } from './gql/gql'
+// const allFilmsWithVariablesQueryDocument = graphql(/* GraphQL */ `
+//   query allFilmsWithVariablesQuery($first: Int!) {
+//     allFilms(first: $first) {
+//       edges {
+//         node {
+//           id
+//           title
+//         }
+//       }
+//     }
+//   }
+// `)
+
 function App() {
   const [count, setCount] = useState(0)
+
+  // const { data } = useQuery({
+  //   queryKey: ['films'],
+  //   queryFn: async () =>
+  //     request(
+  //       import.meta.env.VITE_BACKEND_URI,
+  //       allFilmsWithVariablesQueryDocument,
+  //       // variables are type-checked too!
+  //       { first: 10 },
+  //     ),
+  // })
 
   return (
     <>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  head:[
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/vitepress-logo-mini.svg' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.ico'}],
+    ['meta', { name: 'theme-color', content: '#00bf30' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:locale', content: 'en' }],
+    ['meta', { property: 'og:title', content: 'Pepeunit | Federative IoT Platform' }],
+    ['meta', { property: 'og:site_name', content: 'Pepeunit' }],
+    ['meta', { property: 'og:description', content: 'Federative IoT Platform' }],
+    ['meta', { property: 'og:image', content: process.env.VITE_SELF_URI + '/pepeunit-og.jpg' }],
+    ['meta', { property: 'og:url', content: process.env.VITE_SELF_URI }],
+  ],
+
 })
