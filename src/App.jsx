de4b37d90a0f22a7d110eb37c8d5e403f98211c2
Ivan Serebrennikov <admin@silberworks.com>
2024-08-02 01:39:07 +0300

    feat(icon): icon, name and add dotenv

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import ForceGraph from './components/ForceGraph'
 import './App.css'
 
