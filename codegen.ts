d7a63fdc1a1a83f8db6fc185df12f9e900d26800
Ivan Serebrennikov <admin@silberworks.com>
2024-08-08 06:09:11 +0300

    feat(apollo): add

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
  
 const config: CodegenConfig = {
-  schema: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  schema: 'https://devunit.pepeunit.com/pepeunit/graphql',
   documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
