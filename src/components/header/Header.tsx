29a073d0a49fc11e5b592917fda1a8920b844f56
Ivan Serebrennikov <admin@silberworks.com>
2024-08-18 00:48:12 +0300

    feat(header, modal): change promise use logic, add close to base modal

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,14 +5,23 @@ import signin from '/images/signin.svg'
 import { useState } from 'react'
 
 export default function Header(){
-
     const [isModalSignInOpen, setIsModalSignInOpen] = useState(false)
     const [isModalRegisterOpen, setIsModalRegisterOpen] = useState(false)
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
     const [loginReg, setLoginReg] = useState('')
     const [passwordReg, setPasswordReg] = useState('')
+    const [confirmPassReg, setConfirmPassReg] = useState('') 
 
+    function isValidLogin(login: string) {
+        const regex = /^[a-zA-Z0-9_.-]$/; 
+        return regex.test(login) && login.length >= 8 && login.length <= 100;
+    };
+  
+    function isValidPassword(password: string) {
+        const regex = /^[a-zA-Z0-9_.-]$/; 
+        return regex.test(password) && password.length >= 4 && password.length <= 20;
+    };
 
     function openModalSignIn(){
         setIsModalRegisterOpen(false)
@@ -23,30 +32,56 @@ export default function Header(){
         setIsModalRegisterOpen(true)
     }
 
-    const [createUserMutation, createUserData] = useCreateUserMutation({
+    const [createUserMutation] = useCreateUserMutation({
         variables: {
             login: loginReg,
             password: passwordReg
         },
     });
     
-    console.log(createUserData)
-
     const [
         getToken,
-        tokenData
     ] = useGetTokenLazyQuery({
         variables: {
             credentials: login,
             password: password,
         },
     });
-    
-    if (tokenData.data !== undefined && tokenData.data !== null){
-        localStorage.removeItem('token');
-        localStorage.setItem('token', tokenData.data.getToken);
-        console.log(localStorage.getItem('token'))
-    }
+
+    const handleLogin = () => {
+        getToken().then(tokenData => {
+
+            console.log(tokenData)
+            if (tokenData.data) { 
+                localStorage.removeItem('token');
+                localStorage.setItem('token', tokenData.data.getToken);
+                console.log(tokenData.data.getToken)
+            } else {
+                console.error('Ошибка получения токена:', tokenData.error);
+            }
+        })
+    };
+
+    const handleRegister = () => {
+        if (passwordReg !== confirmPassReg) {
+            alert('Пароли не совпадают!');
+            return;
+        }
+
+        if (!isValidLogin(loginReg) || !isValidPassword(passwordReg)) {
+            alert("Неверный формат логина или пароля!");
+            return;
+        }
+
+        createUserMutation().then(createUserData => {
+            console.log(createUserData.data)
+
+            if (createUserData.data) {
+                console.log('Пользователь создан:', createUserData.data);
+            }
+        })
+
+    };
 
     return (
         <header>
@@ -65,7 +100,7 @@ export default function Header(){
                 <button className="signin" onClick={openModalSignIn}>
                     <img src={signin} width="32" height="32" alt="Signin"/>
                 </button>
-                <BaseModal open={isModalSignInOpen}>
+                <BaseModal open={isModalSignInOpen} closeModal={() => {setIsModalSignInOpen(false)}}>
                     <button className="signin" onClick={openModalRegister}>
                         Регистрация
                     </button>
@@ -86,11 +121,11 @@ export default function Header(){
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </form>
-                    <button className="signin" onClick={() => getToken()}>
+                    <button className="signin" onClick={handleLogin}>
                         Войти
                     </button>
                 </BaseModal>
-                <BaseModal open={isModalRegisterOpen}>
+                <BaseModal open={isModalRegisterOpen} closeModal={() => {setIsModalRegisterOpen(false)}}>
                     <button className="signin" onClick={openModalSignIn}>
                         Авторизация
                     </button>
@@ -98,20 +133,27 @@ export default function Header(){
                     <form>
                         <input
                             id='login_reg'
-                            type='text_reg'
+                            type='text'
                             placeholder='Login'
                             value={loginReg}
                             onChange={(e) => setLoginReg(e.target.value)}
                         />
                         <input
                             id='password_reg'
-                            type='password_reg'
+                            type='password'
                             placeholder='Password'
                             value={passwordReg}
                             onChange={(e) => setPasswordReg(e.target.value)}
                         />
+                        <input
+                            id='confirm_password_reg'
+                            type='password'
+                            placeholder='Confirm Password'
+                            value={confirmPassReg}
+                            onChange={(e) => setConfirmPassReg(e.target.value)}
+                        />
                     </form>
-                    <button className="register" onClick={() => createUserMutation()}>
+                    <button className="register" onClick={handleRegister}>
                         Зарегистрировать
                     </button>
                 </BaseModal>
--- src/components/modal/BaseModal.tsx
+++ src/components/modal/BaseModal.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import close_img from '/images/close.svg'
 
 interface ModalProps {
     children: React.ReactNode
     open: boolean
+    closeModal: () => void
 }
 
-export default function BaseModal({children, open}: ModalProps) {
+export default function BaseModal({children, open, closeModal}: ModalProps) {
     return ReactDOM.createPortal(
-        <dialog open={open}>{children}</dialog>,
+        <dialog open={open}>
+            <button className="close" onClick={closeModal}>
+                <img src={close_img} width="16" height="16" alt="Close"/>
+            </button>
+            {children}
+        </dialog>,
         document.body
     )
 }
