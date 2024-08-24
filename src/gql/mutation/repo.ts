9c4ae4d32f3702dc18d096584409f31b70ddf57e
Ivan Serebrennikov <admin@silberworks.com>
2024-08-25 01:32:45 +0300

    feat(repo): create add

---
+++ src/gql/mutation/repo.ts
@@ -0,0 +1,34 @@
+import { gql } from 'graphql-tag';
+
+gql`
+    mutation createRepo(
+        $visibilityLevel: VisibilityLevel!
+        $name: String!
+        $repoUrl: String!
+        $isPublicRepository: Boolean!
+        credentials: CredentialsInput!
+    ) {
+        createRepo (
+            repo: {
+                visibilityLevel: $visibilityLevel
+                name: $name
+                repoUrl: $repoUrl
+                isPublicRepository: $isPublicRepository
+                credentials: $credentials
+            }
+        ){
+            uuid
+            visibilityLevel
+            name
+            createDatetime
+            repoUrl
+            isPublicRepository
+            isCredentialsSet
+            defaultBranch
+            isAutoUpdateRepo
+            lastUpdateDatetime
+            branches
+            creatorUuid
+        }
+    }
+`
