7fdbd2c926327906f4ea73148f38b54ee1476e69
Ivan Serebrennikov <admin@silberworks.com>
2024-09-01 22:41:57 +0300

    feat(repo): add local update, add units firmware update

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,4 +1,5 @@
-import { useGetReposLazyQuery } from '../../types/composition-functions'
+import { ResultType } from '../../types/resultEnum'
+import { useGetReposLazyQuery, useUpdateLocalRepoMutation, useUpdateUnitsFirmwareMutation } from '../../types/composition-functions'
 import BaseModal from '../modal/BaseModal'
 import { ForceGraph3D } from 'react-force-graph';
 import UpdateRepoForm from '../forms/repo/UpdateRepoForm';
@@ -6,6 +7,8 @@ import UpdateRepoCredentialsForm from '../forms/repo/UpdateRepoCredentialsForm'
 import { RepoType } from '../../types/composition-functions'
 import SpriteText from 'three-spritetext';
 import { useState, useMemo, useCallback, useEffect } from 'react';
+import Spinner from '../forms/primitives/Spinner'
+import ResultQuery from '../forms/primitives/ResultQuery'
 
 interface MainContentProps {
   activeModal: string | null
@@ -17,8 +20,70 @@ export default function MainContent({activeModal, setActiveModal}: MainContentPr
   const [currentRepoData, setCurrentRepoData] = useState<RepoType | null>(null)
   const [displayWidth, setDisplayWidth] = useState(window.innerWidth);
   const [displayHeight, setDisplayHeight] = useState(window.innerHeight);
+
+  const [isLoaderActive, setIsLoaderActive] = useState(false)
+  const [resultData, setResultData] = useState<{ type: ResultType; message: string | null }>({
+    type: ResultType.Happy,
+    message: null
+});
+  
   const [getRepos] = useGetReposLazyQuery();
+  const [updateLocalRepo] = useUpdateLocalRepoMutation();
+  const [updateUnitsFirmware] = useUpdateUnitsFirmwareMutation()
+
   const forceData = useMemo(() => test(), [reposData])
+
+  const handleUpdateLocalRepo = () => {
+    setIsLoaderActive(true)
+    setResultData({
+      ...resultData,
+      message: null
+    })
+
+    if (currentRepoData){
+      updateLocalRepo(
+        {
+          variables: {
+            uuid: currentRepoData.uuid
+          }
+        }
+      ).then(result => {
+        if (result.data){
+          setIsLoaderActive(false)
+          setResultData({ type: ResultType.Happy, message: "Запрос обновления отправлен"})
+        }
+      }).catch(error => {
+        setIsLoaderActive(false)
+        setResultData({ type: ResultType.Angry, message: error.graphQLErrors[0].message.slice(4)})
+      })
+    }
+  };
+
+  const handleUpdateUnitsFirmware = () => {
+    setIsLoaderActive(true)
+    setResultData({
+      ...resultData,
+      message: null
+    })
+
+    if (currentRepoData){
+      updateUnitsFirmware(
+        {
+          variables: {
+            uuid: currentRepoData.uuid
+          }
+        }
+      ).then(result => {
+        if (result.data){
+          setIsLoaderActive(false)
+          setResultData({ type: ResultType.Happy, message: "Запрос обновления связанных Unit отправлен"})
+        }
+      }).catch(error => {
+        setIsLoaderActive(false)
+        setResultData({ type: ResultType.Angry, message: error.graphQLErrors[0].message.slice(4)})
+      })
+    }
+  };
 
   const openModal = useCallback((modalType: string) => {
     setActiveModal(modalType);
@@ -27,7 +92,6 @@ export default function MainContent({activeModal, setActiveModal}: MainContentPr
   const closeModal = useCallback(() => {
       setActiveModal(null);
   }, []);
-
 
   function kek(node: any){
     openModal("repoMenu")
@@ -80,19 +144,26 @@ export default function MainContent({activeModal, setActiveModal}: MainContentPr
         closeModal={closeModal}
       >
         <div className="modal_menu_content">
+          {
+            isLoaderActive && (<Spinner/>)
+          }
           <button className="button_open_alter" onClick={() => openModal('createUnit')}>
             Создать Unit
           </button>
           <button className="button_open_alter" onClick={() => openModal('repoSettingsMenu')}>
             Настройки
           </button>
-          <button className="button_open_alter" onClick={() => openModal('updateLocalRepo')}>
+          <button className="button_open_alter" onClick={handleUpdateLocalRepo}>
             Обновить доступные версии
           </button>
-          <button className="button_open_alter" onClick={() => openModal('updateUnitsFirmware')}>
+          <button className="button_open_alter" onClick={handleUpdateUnitsFirmware}>
             Обновить связанные Unit
           </button>
+          <ResultQuery
+            resultData={resultData}
+          />
         </div>
+
       </BaseModal>
       <BaseModal
         modalName='Настройки'
