4161805358a1799b72960ca397c5684fc5f03a03
Ivan Serebrennikov <admin@silberworks.com>
2024-09-08 18:38:41 +0300

    feat(unit): add units get

---
+++ src/gql/queries/unit.ts
@@ -0,0 +1,42 @@
+import { gql } from 'graphql-tag';
+
+gql`
+    query getUnits(
+        $creatorUuid: UUID
+        $repoUuid: UUID
+        $searchString: String
+        $isAutoUpdateFromRepoUnit: Boolean
+        $visibilityLevel: [VisibilityLevel!]
+        $orderByCreateDate: OrderByDate
+        $orderByLastUpdate: OrderByDate
+        $offset: Int
+        $limit: Int
+    ) {
+        getUnits(
+            filters: {
+                creatorUuid: $creatorUuid
+                repoUuid: $repoUuid
+                searchString: $searchString
+                isAutoUpdateFromRepoUnit: $isAutoUpdateFromRepoUnit
+                visibilityLevel: $visibilityLevel
+                orderByCreateDate: $orderByCreateDate
+                orderByLastUpdate: $orderByLastUpdate
+                offset: $offset
+                limit: $limit
+            }
+        ) {
+            uuid
+            visibilityLevel
+            name
+            createDatetime
+            isAutoUpdateFromRepoUnit
+            repoBranch
+            repoCommit
+            unitStateDict
+            currentCommitVersion
+            lastUpdateDatetime
+            creatorUuid
+            repoUuid
+        }
+    }
+`
