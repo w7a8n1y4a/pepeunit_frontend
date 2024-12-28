62b055488c264a48987180881563efdc043f7043
Ivan Serebrennikov <admin@silberworks.com>
2024-12-28 14:08:41 +0300

    feat(search): change postion components

--- src/components/forms/search/searchForm.tsx
+++ src/components/forms/search/searchForm.tsx
@@ -1,42 +1,35 @@
 import {
-    PermissionEntities,
-    useCreatePermissionMutation
-
- } from '@rootTypes/compositionFunctions';
-import { useResultHandler } from '@rootTypes/useResultHandler';
-import { useAsyncHandler } from '@rootTypes/useAsyncHandler';
-import { useState, useEffect } from 'react';
-import DefaultInput from '@primitives/defaultInput'
-import Spinner from '@primitives/spinner';
-import ResultQuery from '@primitives/resultQuery';
-import PaginationControls from '@primitives/pagination';
-import IterationList from '@primitives/iterationList'
-import EntityTypeSelector from '@primitives/entityTypeSelector';
-import useFetchEntitiesByFilter from '../utils/useFetchEntitiesByFilter';
-import '../form.css';
-
-import { useNodeStore } from '@stores/baseStore';
-
-
-interface PermissionCreateFormProps {
-    currentNodeType: PermissionEntities;
-    selectedEntityType: PermissionEntities;
-    setSelectedEntityType: (show: PermissionEntities) => void;
-}
-
-export default function SearchForm({ currentNodeType, selectedEntityType, setSelectedEntityType }: PermissionCreateFormProps) {
-    const { resultData, setResultData, handleError, handleSuccess } = useResultHandler();
+    PermissionEntities
+  
+  } from '@rootTypes/compositionFunctions';
+  import { useResultHandler } from '@rootTypes/useResultHandler';
+  import { useAsyncHandler } from '@rootTypes/useAsyncHandler';
+  import { useState, useEffect } from 'react';
+  import DefaultInput from '@primitives/defaultInput'
+  import Spinner from '@primitives/spinner';
+  import ResultQuery from '@primitives/resultQuery';
+  import PaginationControls from '@primitives/pagination';
+  import IterationList from '@primitives/iterationList'
+  import EntityTypeSelector from '@primitives/entityTypeSelector';
+  import useFetchEntitiesByFilter from '../utils/useFetchEntitiesByFilter';
+  import '../form.css';
+  
+  import { useNodeStore } from '@stores/baseStore';
+  
+  export default function SearchForm(){
+    const { resultData, setResultData, handleError } = useResultHandler();
     const { isLoaderActive, runAsync } = useAsyncHandler(handleError);
-
+  
+    const [selectedEntityType, setSelectedEntityType] = useState<PermissionEntities>(PermissionEntities.User);
+  
     const { currentNodeData } = useNodeStore();
     
     const [ searchString, setSearchString ] = useState('');
     const [ typeList, setTypeList ] = useState<'button' | 'collapse'>('button');
     const [nodeOutputs, setNodeOutputs] = useState<Array<any> | null>(null);
-
-    const [createPermissionMutation] = useCreatePermissionMutation();
+  
     const { fetchEntitiesByFilter } = useFetchEntitiesByFilter();
-
+  
     const [errorState, setErrorState] = useState({
         searchString: false,
     });
@@ -44,7 +37,7 @@ export default function SearchForm({ currentNodeType, selectedEntityType, setSel
     const [currentPage, setCurrentPage] = useState(0);
     const [totalCount, setTotalCount] = useState(0);
     const itemsPerPage = 6;
-
+  
     const loadEntities = async (searchString: string, selectedEntityType: PermissionEntities, currentPage: number) => {
         runAsync(async () => {
             let result = await fetchEntitiesByFilter(searchString, selectedEntityType, itemsPerPage, currentPage * itemsPerPage)
@@ -78,81 +71,70 @@ export default function SearchForm({ currentNodeType, selectedEntityType, setSel
             }
         })
     };
-
+  
     useEffect(() => {
         loadEntities('', selectedEntityType, currentPage);
     }, [currentNodeData]);
-
+  
     useEffect(() => {
         loadEntities(searchString, selectedEntityType, currentPage);
     }, [searchString, currentPage, selectedEntityType]);
-
+  
     const updateErrorState = (field: keyof typeof errorState, hasError: boolean) => {
         setErrorState(prevState => ({
             ...prevState,
             [field]: hasError
         }));
     };
-
+  
     const handleCreatePermission = (entityUuid: string) => {
-        runAsync(async () => {
-            let result = await createPermissionMutation({
-                variables: {
-                    agentUuid: entityUuid,
-                    agentType: selectedEntityType,
-                    resourceUuid: currentNodeData.uuid,
-                    resourceType: currentNodeType
-                }
-            })
-            if (result.data){
-                handleSuccess("Permission success create")
-            }
-        })
+      console.log(entityUuid)
     };
-
+  
     const totalPages = Math.ceil(totalCount / itemsPerPage);
-
+  
     return (
-        <>
-            {isLoaderActive && <Spinner />}
+      <>
+        {isLoaderActive && <Spinner />}
 
-            <form>
-                <DefaultInput
-                    id="name_set"
-                    type="text"
-                    placeholder="Search name"
-                    value={searchString}
-                    validateState={searchString}
-                    onChange={setSearchString}
-                    validateFunc={() => (null)}
-                    setIsErrorExist={(hasError) => updateErrorState('searchString', hasError)}
-                    setResultData={setResultData}
-                />
-            </form>
-
-            <EntityTypeSelector
-                entities={[PermissionEntities.User, PermissionEntities.Unit, PermissionEntities.UnitNode]}
-                selectedEntityType={selectedEntityType}
-                setSelectedEntityType={setSelectedEntityType}
+        <form>
+            <DefaultInput
+                id="name_set"
+                type="text"
+                placeholder="Search"
+                value={searchString}
+                validateState={searchString}
+                onChange={setSearchString}
+                validateFunc={() => (null)}
+                setIsErrorExist={(hasError) => updateErrorState('searchString', hasError)}
+                setResultData={setResultData}
             />
+        </form>
 
-            <IterationList
-                items={nodeOutputs}
-                renderType={typeList}
-                handleCreate={handleCreatePermission}
-                openModalName={null}
-            />
+        <EntityTypeSelector
+            entities={[PermissionEntities.User, PermissionEntities.Repo, PermissionEntities.Unit, PermissionEntities.UnitNode]}
+            selectedEntityType={selectedEntityType}
+            setSelectedEntityType={setSelectedEntityType}
+        />
 
-            <PaginationControls
-                currentPage={currentPage}
-                totalPages={totalPages}
-                goToNextPage={() => setCurrentPage(prev => prev + 1)}
-                goToPreviousPage={() => setCurrentPage(prev => prev - 1)}
-            />
+        <IterationList
+            items={nodeOutputs}
+            renderType={typeList}
+            handleCreate={handleCreatePermission}
+            openModalName={null}
+        />
 
-            <ResultQuery
-                resultData={resultData}
-            />
-        </>
-    );
-}
+        <PaginationControls
+            currentPage={currentPage}
+            totalPages={totalPages}
+            goToNextPage={() => setCurrentPage(prev => prev + 1)}
+            goToPreviousPage={() => setCurrentPage(prev => prev - 1)}
+        />
+
+        <ResultQuery
+            resultData={resultData}
+        />
+      </>
+    )
+  }
+  
