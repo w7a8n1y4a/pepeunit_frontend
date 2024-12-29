edd693312dabe5e93033adef1e45b7f0587cf1f3
Ivan Serebrennikov <admin@silberworks.com>
2024-12-30 02:40:31 +0300

    feat(search): add base mod system, add button current target item, add backward to all graph

--- src/components/stores/baseStore.ts
+++ src/components/stores/baseStore.ts
@@ -10,7 +10,6 @@ export const useModalStore = create<ModalStore>((set) => ({
     setActiveModal: (show: string | null) => set({ activeModal: show }),
 }));
 
-
 export interface NodeStore {
     currentNodeData: any | null,
     setCurrentNodeData: (unit: any | null) => void,
@@ -19,4 +18,24 @@ export interface NodeStore {
 export const useNodeStore = create<NodeStore>((set) => ({
     currentNodeData: null as any | null,
     setCurrentNodeData: (node: any | null ) => set({ currentNodeData: node }),
+}));
+
+export interface SearchNodeStore {
+    currentSearchNodeData: any | null,
+    setCurrentSearchNodeData: (unit: any | null) => void,
+}
+
+export const useSearchNodeStore = create<SearchNodeStore>((set) => ({
+    currentSearchNodeData: null as any | null,
+    setCurrentSearchNodeData: (node: any | null ) => set({ currentSearchNodeData: node }),
+}));
+
+export interface ReloadBaseGraphDataStore {
+    reloadState: boolean,
+    setReloadState: (state: boolean) => void,
+}
+
+export const useReloadBaseGraphDataStore = create<ReloadBaseGraphDataStore>((set) => ({
+    reloadState: false,
+    setReloadState: (state: boolean) => set({ reloadState:  state}),
 }));
