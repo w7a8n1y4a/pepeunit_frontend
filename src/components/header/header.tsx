5b6b7ab1b38ed9b7283d22b85a15d4c0e9e73533
Ivan Serebrennikov <admin@silberworks.com>
2024-12-30 02:46:07 +0300

    feat(style): add style for base mod menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -52,16 +52,18 @@ export default function Header(){
                     Docs
                 </a>
             </div>
-            <button aria-label="Search" className="search-button" onClick={() => openModal('graphSearch')}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" width="20" height="20">
-                    <path d="M11 4a7 7 0 1 1 0 14a7 7 0 0 1 0-14zm0-2a9 9 0 1 0 6.219 15.546l4.396 4.395l1.414-1.414l-4.395-4.396A9 9 0 0 0 11 2z" fill="currentColor"/>
-                </svg>
-                Search
-            </button>
+            <div className="search-div">
+                <button aria-label="Search" className="search-button" onClick={() => openModal('graphSearch')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" width="20" height="20">
+                        <path d="M11 4a7 7 0 1 1 0 14a7 7 0 0 1 0-14zm0-2a9 9 0 1 0 6.219 15.546l4.396 4.395l1.414-1.414l-4.395-4.396A9 9 0 0 0 11 2z" fill="currentColor"/>
+                    </svg>
+                    Search
+                </button>
+            </div>
 
             {
                 currentSearchNodeData && (
-                    <div >
+                    <div className="search-mod-menu">
                         <button className="search-button" onClick={() => {setReloadState(!reloadState)}}>
                             <img src={back_img} width="20" height="20" alt="Back"/>
                         </button>
