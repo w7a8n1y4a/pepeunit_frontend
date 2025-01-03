ac93f3ddb92a0656b8ed2bece9b9998d59736f4f
Ivan Serebrennikov <admin@silberworks.com>
2025-01-03 18:40:43 +0300

    feat(perm, update): add update by icon in modal and perm del UnitNode

--- src/components/modal/baseModal.tsx
+++ src/components/modal/baseModal.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { NodeType } from '@rootTypes/nodeTypeEnum'
-import { UnitNodeTypeEnum } from '@rootTypes/compositionFunctions'
+import { UnitNodeTypeEnum, useGetRepoLazyQuery, useGetUnitLazyQuery, useGetUnitNodeLazyQuery } from '@rootTypes/compositionFunctions'
+import { useResultHandler } from '@rootTypes/useResultHandler';
+import { useAsyncHandler } from '@rootTypes/useAsyncHandler';
 import close_img from '/images/close.svg'
 import back_img from '/images/back.svg'
 import reload_img from '/images/reload.svg'
 import './baseModal.css'
 
+import { useGraphStore } from '@stores/graphStore';
+
 import useModalHandlers from '@handlers/useModalHandlers';
+import { useNodeStore } from '@stores/baseStore';
 
 interface ModalProps {
     modalName: string
@@ -19,6 +24,50 @@ interface ModalProps {
 
 export default function BaseModal({modalName, children, open, openModalType, reloadEntityType}: ModalProps) {
     const { openModal, closeModal } = useModalHandlers();
+    const { handleError } = useResultHandler();
+    const { runAsync } = useAsyncHandler(handleError);
+    const { currentNodeData, setCurrentNodeData } = useNodeStore();
+
+    const {graphData, updateNodeDataById } = useGraphStore();
+
+    const [getRepo] = useGetRepoLazyQuery();
+    const [getUnit] = useGetUnitLazyQuery();
+    const [getUnitNode] = useGetUnitNodeLazyQuery();
+
+    function updateData(reloadEntityType: NodeType | UnitNodeTypeEnum) {
+        runAsync(async () => {
+            let targetdata = null
+            if (reloadEntityType == NodeType.Repo){
+                console.log(reloadEntityType)
+                let result = await getRepo({
+                    variables: {uuid: currentNodeData.uuid}
+                })
+                targetdata = result.data?.getRepo
+            }
+    
+            if (reloadEntityType == NodeType.Unit){
+                let result = await getUnit({
+                    variables: {uuid: currentNodeData.uuid}
+                })
+                targetdata = result.data?.getUnit
+            }
+    
+            if (reloadEntityType == UnitNodeTypeEnum.Input || reloadEntityType == UnitNodeTypeEnum.Output){
+                let result = await getUnitNode({
+                    variables: {uuid: currentNodeData.uuid}
+                })
+                targetdata = result.data?.getUnitNode
+                targetdata = {...targetdata, name: targetdata?.topicName}
+            }
+
+            if (targetdata?.uuid){
+                updateNodeDataById(targetdata.uuid, targetdata)
+                console.log(graphData)
+                setCurrentNodeData(targetdata)
+            }
+
+        })
+    }
     
     return ReactDOM.createPortal(
         <dialog open={open}>
@@ -29,8 +78,8 @@ export default function BaseModal({modalName, children, open, openModalType, rel
                 </div>
                 <div className="div_modal_buttons">
                     {
-                        reloadEntityType && openModal && (
-                            <button className="modal_menu_button" onClick={() => openModal(reloadEntityType)}>
+                        reloadEntityType && currentNodeData && (
+                            <button className="modal_menu_button" onClick={() => updateData(reloadEntityType)}>
                                 <img src={reload_img} width="20" height="20" alt="Reload"/>
                             </button>
                         )
