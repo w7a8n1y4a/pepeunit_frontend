4df784e11cbf10a0ee33886b2b45a6c11da3bb90
Ivan Serebrennikov <admin@silberworks.com>
2025-01-29 07:46:29 +0300

    feat(search): add truncate, add correct check unitNode, add clicable all types unitNodes

---
+++ src/utils/truncateString.ts
@@ -0,0 +1,6 @@
+const truncateText = (text: string, maxLength: number): string => {
+    if (text.length <= maxLength) return text;
+    return text.slice(0, maxLength) + "..";
+};
+
+export default truncateText;
