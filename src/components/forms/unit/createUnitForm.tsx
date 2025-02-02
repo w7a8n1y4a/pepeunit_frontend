3ae071f79f2e00919afbb5887f26a33f942d62eb
Ivan Serebrennikov <admin@silberworks.com>
2025-02-03 02:20:26 +0300

    feat(compile): state managment change

--- src/components/forms/unit/createUnitForm.tsx
+++ src/components/forms/unit/createUnitForm.tsx
@@ -150,7 +150,7 @@ export default function CreateUnitForm() {
                 }
             })
         }
-    }, [repoCommit, isAutoUpdateFromRepoUnit]);
+    }, [currentNodeData, repoCommit, isAutoUpdateFromRepoUnit]);
 
 
     return (
